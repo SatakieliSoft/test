c95e37309f748acea49944804c36238409192c99
SatakieliSoft <cestasvmartina@gmail.com>
2025-06-23 11:13:59 +0200

    Update front.js

--- js/front.js
+++ js/front.js
@@ -81,3 +81,33 @@ canvas.addEventListener('wheel', (e) => {
 nextButton.addEventListener('click', () => {
   alert("Pokračujeme na zadnú stranu pohľadnice.");
 });
+
+// Facebook SDK a zdieľanie
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1234567890', // TODO: Nahraď vlastné App ID
+    xfbml      : true,
+    version    : 'v18.0'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/sk_SK/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+document.getElementById("share-button").addEventListener("click", function() {
+  FB.ui({
+    method: 'share',
+    href: window.location.href
+  }, function(response) {
+    if (!response || response.error_message) {
+      alert('Zdieľanie zlyhalo.');
+    } else {
+      alert('Zdieľanie prebehlo úspešne!');
+    }
+  });
+});
